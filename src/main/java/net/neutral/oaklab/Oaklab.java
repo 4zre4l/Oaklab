525a4198e71e2db23932410f2ba25e5f95482277
Ciaran <ciaranjeastwood@gmail.com>
2025-06-25 01:57:09 +0100

    Fucked up adding an item

--- src/main/java/net/neutral/oaklab/Oaklab.java
+++ src/main/java/net/neutral/oaklab/Oaklab.java
@@ -2,6 +2,7 @@ package net.neutral.oaklab;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.neutral.oaklab.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class Oaklab implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
